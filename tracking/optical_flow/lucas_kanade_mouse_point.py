e7ee114c2b263a68c7bbec44749ad833a416b985
Chan Hiep Luong <hiep.lchan@gmail.com>
2019-01-25 15:42:06 +0700

    edit lucas_kanade_mouse_point.py

--- tracking/optical_flow/lucas_kanade_mouse_point.py
+++ tracking/optical_flow/lucas_kanade_mouse_point.py
@@ -1,18 +1,18 @@
 import cv2
 import numpy as np
- 
+
 vid_num = 1
 cap = cv2.VideoCapture("/home/hiep/Tracking_CCTV/CCTV_Data/video/" + str(vid_num) + ".mp4")
- 
+
 # Create old frame
 _, frame = cap.read()
 old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
- 
+
 # Lucas kanade params
 lk_params = dict(winSize = (15, 15),
                  maxLevel = 4,
                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
- 
+
 # Mouse function
 def select_point(event, x, y, flags, params):
     global point, point_selected, old_points
@@ -20,34 +20,46 @@ def select_point(event, x, y, flags, params):
         point = (x, y)
         point_selected = True
         old_points = np.array([[x, y]], dtype=np.float32)
- 
+
 cv2.namedWindow("Frame")
 cv2.setMouseCallback("Frame", select_point)
- 
+
 point_selected = False
 point = ()
 old_points = np.array([[]])
 while True:
     _, frame = cap.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
- 
+
     if point_selected is True:
         cv2.circle(frame, point, 5, (0, 0, 255), 2)
- 
+
         new_points, status, error = cv2.calcOpticalFlowPyrLK(old_gray, gray_frame, old_points, None, **lk_params)
         old_gray = gray_frame.copy()
         old_points = new_points
- 
+
         x, y = new_points.ravel()
         cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
- 
- 
- 
-    cv2.imshow("Frame", frame)
- 
+
+        # Select good points
+        good_new = new_points[st==1]
+        good_old = old_points[st==1]
+
+        # draw the tracks
+        for i,(new,old) in enumerate(zip(good_new,good_old)):
+            a,b = new.ravel()
+            c,d = old.ravel()
+            mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+            frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+
+        img = cv2.add(frame,mask)
+
+
+    cv2.imshow("Frame", img)
+
     key = cv2.waitKey(1)
     if key == 27:
         break
- 
+
 cap.release()
 cv2.destroyAllWindows()
