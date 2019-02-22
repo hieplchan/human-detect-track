8cb0c582ec3fe9eaaf818c8225471df1966b8c7d
hiep.lchan <hiep.lchan@gmail.com>
2019-02-22 17:12:18 +0700

    Done posenet package (model memory basic optimize) TODO: + posenet interface for change parameters + gpu workers for multi processing + posenet worker object ????

--- production/posenet_package/posenet/evaluate.py
+++ production/posenet_package/posenet/evaluate.py
@@ -29,17 +29,17 @@ def getResultPointBox(model, draw_image):
         for ks, kc in zip(keypoint_scores[ii, :], keypoint_coords[ii, :, :]):
             if ks < THRESHOLD:
                 continue
-            cv_keypoints.append(cv2.KeyPoint(kc[1]/2, kc[0]/2, 10.))
+            cv_keypoints.append(cv2.KeyPoint(kc[1], kc[0], 10.))
 
     return cv_keypoints, boxs
 
 def drawResultBox(draw_image, boxs):
     for box in boxs:
-        boxs_image = cv2.rectangle(draw_image, (box[1], box[3]), (box[0], box[2]), (0,255,0), 3)
-    return boxs_image
+        draw_image = cv2.rectangle(draw_image, (box[1], box[3]), (box[0], box[2]), (0,255,0), 3)
+    return draw_image
 
 def drawResultPoint(draw_image, cv_keypoints):
     if cv_keypoints:
-        points_image = cv2.drawKeypoints(draw_image, cv_keypoints, outImage=np.array([]), color=(255, 255, 0),
+        draw_image = cv2.drawKeypoints(draw_image, cv_keypoints, outImage = np.array([]), color = (255, 255, 0),
                                         flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    return points_image
+    return draw_image
--- production/posenet_package/posenet_package_test.py
+++ production/posenet_package/posenet_package_test.py
@@ -10,25 +10,23 @@ cap = cv2.VideoCapture('/home/hiep/Desktop/Tracking_CCTV/CCTV_Data/Video/1.mp4')
 # Posenet model setting and load
 posenet.MODEL_PATH = '/home/hiep/Desktop/Tracking_CCTV/production/posenet_package/posenet/_models/mobilenet_v1_050_gpu.pth'
 
-# posenet.params_reconfig()
 model = posenet.load(posenet.MODEL_PATH, posenet.OUTPUT_STRIDE, posenet.DEVICE)
 
 if __name__ == "__main__":
     with torch.no_grad():
+        frame_num = 0
         while (True):
             res, draw_image = cap.read()
-
             start = time.time()
-            # Posenet
+
             resultPoints, resultBoxs = posenet.getResultPointBox(model, draw_image)
-            boxs_image = posenet.drawResultBox(draw_image, resultBoxs)
-            points_image = posenet.drawResultPoint(draw_image, resultPoints)
+            decoded_image = posenet.drawResultBox(draw_image, resultBoxs)
+            decoded_image = posenet.drawResultPoint(draw_image, resultPoints)
 
             stop = time.time()
-
             print((stop - start)*1000)
 
-            cv2.imwrite('boxs_image.jpg', boxs_image)
-            cv2.imwrite('points_image.jpg', points_image)
+            frame_num = frame_num + 1
+            cv2.imwrite('output/' + str(frame_num) + '.jpg', decoded_image)
 
-    time.sleep(60)
+        time.sleep(60)
