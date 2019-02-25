b1abb38f28b75e530570627f7d68eb398e8937be
hiep.lchan <hiep.lchan@gmail.com>
2019-02-25 10:55:27 +0700

    posenet raspberry basic done + 4.5 - 5 s/frame

---
+++ production/posenet_package_raspberry/README.md
@@ -0,0 +1,2 @@
+1. PYTHON ENVS SETUP:
+[Pytorch Raspberry Install](https://medium.com/hardware-interfacing/how-to-install-pytorch-v4-0-on-raspberry-pi-3b-odroids-and-other-arm-based-devices-91d62f2933c7)
--- production/posenet_package_raspberry/test_posenet_package.py
+++ production/posenet_package_raspberry/test_posenet_package.py
@@ -5,25 +5,20 @@ import torch
 import posenet
 
 #Video load for test
-cap = cv2.VideoCapture('/home/hiep/Desktop/Tracking_CCTV/CCTV_Data/Video/1.mp4')
+cap = cv2.VideoCapture('/home/hiep/Tracking_CCTV/CCTV_Data/Video/1.mp4')
 
 # Posenet model setting and load
-posenet.MODEL_PATH = '/home/hiep/Desktop/Tracking_CCTV/production/posenet_package/posenet/_models/mobilenet_v1_050_gpu.pth'
+posenet.MODEL_PATH = '/home/hiep/Tracking_CCTV/production/posenet_package_raspberry/posenet/_models/mobilenet_v1_050.pth'
 
 model = posenet.load(posenet.MODEL_PATH, posenet.OUTPUT_STRIDE, posenet.DEVICE)
 
-
 if __name__ == "__main__":
-    for name, param in model.named_parameters():
-        if param.requires_grad:
-            print (name, param.data.type())
-
     with torch.no_grad():
         frame_num = 0
         while (True):
             res, draw_image = cap.read()
             start = time.time()
-
+            # posenet.getResultPointBox(model, draw_image)
             resultPoints, resultBoxs = posenet.getResultPointBox(model, draw_image)
             decoded_image = posenet.drawResultBox(draw_image, resultBoxs)
             decoded_image = posenet.drawResultPoint(draw_image, resultPoints)
@@ -31,7 +26,7 @@ if __name__ == "__main__":
             stop = time.time()
             print((stop - start)*1000)
 
-            # frame_num = frame_num + 1
-            # cv2.imwrite('output/' + str(frame_num) + '.jpg', decoded_image)
+            frame_num = frame_num + 1
+            cv2.imwrite('output/' + str(frame_num) + '.jpg', decoded_image)
 
-    time.sleep(60)
+        time.sleep(60)
